2dea0a4a3ceb8a4dc16268e6ccbdc11342bf8a51
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2018-08-13 08:29:29 -0500

    Define rect struct

---
+++ rect.go
@@ -0,0 +1,11 @@
+package lsd
+
+type rect struct {
+	x1, y1, x2, y2   float64 // Endpoints of the line segment
+	width            float64
+	centerX, centerY float64 // Center of the rectangle
+	theta            float64 // Angle
+	dx, dy           float64 // (dx, dy) is a vector oriented as the line segment
+	anglePrecision   float64 // Tolerance angle
+	pAligned         float64 // Probability of a point with angle within 'anglePrecision'
+}
