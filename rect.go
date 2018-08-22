0f02ac3fa525e6ae8add50bbfc6531705715c976
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2018-08-21 19:30:20 -0500

    Improve rect struct names and documentation
    - Make it clearer that the center of the rectangle is really a weighted
    average
    - Clarify definition of 'pAligned'

--- rect.go
+++ rect.go
@@ -3,13 +3,13 @@ package lsd
 // TODO: reduce code reuse between rect and LineSegment types
 
 type rect struct {
-	p1, p2         Point // Endpoints of the corresponding line segment
+	p1, p2         Point // Endpoints of the line segment going through 'weightedCenter' and aligned with the vector <'dx', 'dy'>; also, midpoints of opposite sides of the rectangle
 	width          float64
-	center         Point   // Center of the rectangle
+	weightedCenter Point   // Weighted sum of the coordinates of all the pixels in the region, weighted by the norm of the gradient
 	theta          float64 // Angle
-	dx, dy         float64 // (dx, dy) is a vector oriented as the line segment
+	dx, dy         float64 // <'dx', 'dy'> is the rectangle's primary axis
 	anglePrecision float64 // Tolerance angle
-	pAligned       float64 // Probability of a point with angle within 'anglePrecision'
+	pAligned       float64 // Probability of a point in the rectangle having angle value within 'anglePrecision' of 'theta'
 }
 
 /* Since rect is made of primitive types, we do not need to define a method corresponding to rect_copy.
