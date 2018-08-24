7661b77e10a31cef2f10600daa8323155c6818dd
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2018-08-23 23:18:02 -0500

    Refactor level line calculations
    Rename level line calculation method to 'computeGradients'. This moe
    accurately conveys the purpose of the method.
    
    Also combine angle and norm values into gradient struct -- this makes the code cleaner (i.e. function signature of 'computeGradients' is shorter)

--- gradient.go
+++ gradient.go
@@ -1,13 +1,13 @@
 package lsd
 
-// if 'defined' is false, then the value of 'value' is meaningless
-type angle struct {
-	defined bool
-	value   float64
+// if 'norm' is zero, then the value of 'angle' is meaningless
+type gradient struct {
+	norm  bool
+	angle float64
 }
 
 /*
-Compute the direction of the level-line of 'img' at each pixel
+Compute the magnitude and direction of the level-line of 'img' at each pixel
 
 Parameters:
 'img': input grid of gray values at each pixel
@@ -15,11 +15,10 @@ Parameters:
 'bins': number of bins to use in the sorting algorithm
 
 Return:
-'angles': angle at each pixel
-'norms': norm of gradient at each pixel
+'grads': gradient at each pixel
 'descNorm': a channel returning coordinates for pixels roughly ordered by decreasing
 gradient magnitude
 */
-func levelLineAngles(img [][]float64, threshold float64, bins uint32) (angles [][]angle, norms [][]float64, descNorm <-chan Point) {
+func computeGradients(img [][]float64, threshold float64, bins uint32) (grads [][]gradient, descNorm <-chan Point) {
 
 }
